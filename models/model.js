c6efae702ea6c0b67a64bb96c6d1f781adb5b372
chrisdotgrubb <developer.grubb@gmail.com>
2023-06-16 14:03:14 -0400

    Add flight model.

---
+++ models/model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+	airline: {
+		type: String,
+		enum: ['American', 'Southwest', 'United'],
+	},
+	airport: {
+		type: String,
+		enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+		default: 'DEN',
+	},
+	flightNo: {
+		type: Number,
+		minimum: 10,
+		maximum: 9999
+	},
+	departs: {
+		type: Date,
+		default: () => {
+			const today = new Date();
+			today.setFullYear(today.getFullYear() + 1);
+			return today;
+		},
+	},
+})
